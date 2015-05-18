d8a86d2ab63814f0c4cb067aac4a2707d69e2488
Thomas Fehér <thomasfeher@web.de>
2015-05-18 23:13:42 +0200

    current state

--- src/main.cpp
+++ src/main.cpp
@@ -14,7 +14,7 @@ using std::chrono::duration;
 using AudioVec = std::vector<float>;
 using audiofft::AudioFFT;
 using FftwData = std::tuple<AudioFFT& // fft class
-                           ,AudioVec& // input vector
+                           ,std::vector<AudioVec>& // input vector
 						   ,AudioVec& // real vector
 						   ,AudioVec& // imag vector
 						   ,AudioVec&>; // output vector
@@ -24,27 +24,30 @@ namespace {
 	uint32_t fft_size = 48000;
 	uint32_t conv_ir_size = 72000;
 	uint32_t conv_sig_size = 5760000;
+	uint32_t min_block_size = 1024;
+	//uint32_t conv_sig_size = 1024;
+	// when false, the optimal blocksize and the corresponding number of
+	// FFTs is calculated automatically 
+	bool do_in_one_block = true;
 }
 
-std::pair<duration<double>, arma::fvec> Convolution(arma::fvec sig, arma::fvec ir) {
-	size_t size = (::conv_sig_size+::conv_ir_size-1);
+duration<double> Convolution(std::vector<arma::fvec>& sig, arma::fvec ir, size_t fft_size, std::vector<arma::fvec> output) {
 	ir = arma::flipud(ir);
-	arma::fvec sig_new = arma::zeros<arma::fvec>(::conv_sig_size + 2*(::conv_ir_size-1));
-	sig_new.subvec(::conv_ir_size - 1, ::conv_sig_size + ::conv_ir_size -2) = sig;
-	arma::fvec output (::conv_sig_size + ::conv_ir_size - 1, arma::fill::zeros);
 
 	auto begin = high_resolution_clock::now();
-	for (uint32_t cnt=0;cnt<::num;++cnt) {
-		for (uint32_t sample_cnt=0;sample_cnt<size;++sample_cnt) {
+	for (uint32_t cnt=0;cnt<sig.size();++cnt) {
+		arma::fvec sig_new = arma::zeros<arma::fvec>(sig[0].size() + 2*(ir.size()-1));
+		sig_new.subvec(ir.size()-1, sig[cnt].size()+ir.size()-2) = sig[cnt];
+		for (uint32_t sample_cnt=0;sample_cnt<fft_size;++sample_cnt) {
+			output[cnt][sample_cnt] = 0;
 			for (uint32_t ir_cnt=0;ir_cnt<::conv_ir_size;++ir_cnt) {
-				output[sample_cnt] += sig_new[sample_cnt+ir_cnt] * ir[ir_cnt];
+				output[cnt][sample_cnt] += sig_new[sample_cnt+ir_cnt] * ir[ir_cnt];
 			}
 		}
 	}
-	AudioVec output_copy = arma::conv_to<AudioVec>::from(output);
 	auto end = high_resolution_clock::now();
 
-	return std::make_pair(end - begin, output);
+	return end - begin;
 }
 
 std::pair<duration<double>, arma::fvec> ArmadilloConv(arma::fvec sig, arma::fvec ir) {
@@ -60,24 +63,21 @@ std::pair<duration<double>, arma::fvec> ArmadilloConv(arma::fvec sig, arma::fvec
 	return std::make_pair(end - begin, output);
 }
 
-duration<double> FftwConv(FftwData sig, FftwData ir) {
-	auto sig_in = std::get<1>(sig);
-	auto sig_re = std::get<2>(sig);
-	auto sig_im = std::get<3>(sig);
-	auto ir_in = std::get<1>(ir);
-	auto ir_re = std::get<2>(ir);
-	auto ir_im = std::get<3>(ir);
+//duration<double> FftwConv(FftwData sig, FftwData ir) {
+duration<double> FftwConv(AudioFFT sig_fft, const std::vector<AudioVec>& sig_in,AudioVec& sig_re, AudioVec& sig_im, AudioFFT ir_fft, const AudioVec& ir_in, AudioVec& ir_re, AudioVec& ir_im, std::vector<AudioVec>& output) {
 	AudioVec re (sig_re.size());
 	AudioVec im (sig_re.size());
 
 	auto begin = high_resolution_clock::now();
-	std::get<0>(sig).fft(sig_in.data(), sig_re.data(), sig_im.data());
-	std::get<0>(ir).fft(ir_in.data(), ir_re.data(), ir_im.data());
-	for (size_t cnt=0;cnt<sig_re.size();++cnt) {
-		re[cnt] = sig_re[cnt]*ir_re[cnt] - sig_im[cnt]*ir_im[cnt];
-		im[cnt] = sig_re[cnt]*ir_im[cnt] + sig_im[cnt]*ir_re[cnt];
+	ir_fft.fft(ir_in.data(), ir_re.data(), ir_im.data());
+	for (uint32_t block_cnt=0;block_cnt<sig_in.size();++block_cnt) {
+		sig_fft.fft(sig_in[block_cnt].data(), sig_re.data(), sig_im.data());
+		for (size_t cnt=0;cnt<sig_re.size();++cnt) {
+			re[cnt] = sig_re[cnt]*ir_re[cnt] - sig_im[cnt]*ir_im[cnt];
+			im[cnt] = sig_re[cnt]*ir_im[cnt] + sig_im[cnt]*ir_re[cnt];
+		}
+		sig_fft.ifft(output[block_cnt].data(), re.data(), im.data());
 	}
-	std::get<0>(sig).ifft(std::get<4>(sig).data(), re.data(), im.data());
 	auto end = high_resolution_clock::now();
 
 	return end - begin;
@@ -139,6 +139,37 @@ std::tuple<size_t,size_t,uint32_t> CalcSizes(uint32_t ir_size, uint32_t sig_size
 	return std::make_tuple(sig_block_size, fft_size, block_num);
 }
 
+std::tuple<std::vector<arma::fvec>,std::vector<AudioVec>,std::vector<arma::fvec>,std::vector<AudioVec>> PrepareData(const arma::fvec& sig, size_t sig_block_size, size_t ir_size) {
+	std::vector<arma::fvec> sig_vec_arma;
+	std::vector<arma::fvec> out_vec_arma;
+	std::vector<AudioVec> sig_vec;
+	std::vector<AudioVec> out_vec;
+
+	for (uint32_t cnt=0;cnt<sig.size();cnt+=sig_block_size) {
+		size_t end_idx = cnt+sig_block_size-1;
+		if (end_idx > sig.size() - 1) {
+			// last block with zeros appended
+			end_idx = sig.size() -1;
+			// correct sized block with zeros
+			sig_vec_arma.push_back(arma::zeros<arma::fvec>(sig_block_size));
+			sig_vec.push_back(std::vector<float> (sig_block_size,0));
+			// copy last elements of signal to first elements of the last block
+			sig_vec_arma[cnt].subvec(0,sig.size()-cnt+1) = sig(cnt,sig.size()-1);
+			std::copy(sig.begin()+cnt, sig.end(), sig_vec[cnt].begin());
+		}
+		else {
+			sig_vec_arma.push_back(sig.subvec(cnt,end_idx));
+			AudioVec block;
+			block.insert(block.end(), sig.begin()+cnt, sig.begin()+end_idx);
+			sig_vec.push_back(block);
+		}
+		out_vec_arma.push_back(arma::zeros<arma::fvec>(sig_block_size+ir_size-1));
+		out_vec.push_back(AudioVec(sig_block_size+ir_size-1,0));
+	}
+	
+	return std::make_tuple(sig_vec_arma,sig_vec,out_vec_arma,out_vec);
+}
+
 int main(int argc, char* argv[]) {
 
 	std::cout << "generating input signals" << std::endl;
@@ -146,10 +177,21 @@ int main(int argc, char* argv[]) {
 	arma::fvec ir {arma::randn<arma::fvec>(::conv_ir_size)};
 	size_t out_size = ::conv_sig_size + ::conv_ir_size - 1;
 
+	size_t sig_block_size;
+	size_t fft_size;
+	uint32_t block_num;
+	std::tie(sig_block_size,fft_size,block_num) = CalcSizes(::conv_ir_size,
+			                                                ::conv_sig_size,
+															::min_block_size);
+	std::vector<arma::fvec> sig_vec_arma;
+	std::vector<arma::fvec> out_vec_arma;
+	std::vector<AudioVec> sig_vec;
+	std::vector<AudioVec> out_vec;
+	std::tie(sig_vec_arma,sig_vec,out_vec_arma,out_vec) = PrepareData(sig,sig_block_size,::conv_ir_size);
+
 	std::cout << "initializing FFTW" << std::flush;
-	size_t fft_size (pow(2,ceil(log2(out_size))));
-	AudioVec sig_vec  (fft_size, 0);
-	std::copy_n(sig.begin(), ::conv_sig_size, sig_vec.begin());
+	//AudioVec sig_vec  (fft_size, 0);
+	//std::copy_n(sig.begin(), ::conv_sig_size, sig_vec.begin());
 	AudioVec ir_vec (fft_size, 0);
 	std::copy_n(ir.begin(), ::conv_ir_size, ir_vec.begin());
 	AudioVec sig_re(AudioFFT::ComplexSize(fft_size)); 
@@ -158,14 +200,16 @@ int main(int argc, char* argv[]) {
 	AudioVec ir_im(AudioFFT::ComplexSize(fft_size)); 
 	AudioVec output(fft_size);
 	AudioFFT fft_sig;
+	// mearure only one initialization, for the second one we could just use
+	// the wisdom of the first one.
 	auto begin = high_resolution_clock::now();
 	fft_sig.init(fft_size);
 	auto end = high_resolution_clock::now();
 	std::cout << "\n\tduration: " << (std::chrono::duration_cast<duration<double>>(end - begin)).count() << std::endl;
 	AudioFFT fft_ir;
 	fft_ir.init(fft_size);
-	FftwData sig_data = std::forward_as_tuple(fft_sig, sig_vec, sig_re, sig_im, output);
-	FftwData ir_data = std::forward_as_tuple(fft_ir, ir_vec, ir_re, ir_im, output);
+	//FftwData sig_data = std::forward_as_tuple(fft_sig, sig_vec, sig_re, sig_im, output);
+	//FftwData ir_data = std::forward_as_tuple(fft_ir, ir_vec, ir_re, ir_im, output);
 
 	std::cout << "Armadillo FFT: " << std::flush;
 	auto result_arma_fft = ArmadilloFft(sig);
@@ -173,8 +217,8 @@ int main(int argc, char* argv[]) {
 
 	// normal convolution, this is our reference output
 	std::cout << "convolution: " << std::flush;
-	auto result_conv = Convolution(sig, ir);
-	std::cout << "\n\tduration: " << result_conv.first.count() << std::endl;
+	auto result_conv = Convolution(sig_vec_arma, ir, fft_size, out_vec_arma);
+	std::cout << "\n\tduration: " << result_conv.count() << std::endl;
 
 	std::cout << "Armadillo FFT-Pow2-convolution: " << std::flush;
 	auto result_arma_fft_pow2_conv = ArmadilloFftPow2Conv(sig, ir);
@@ -184,7 +228,7 @@ int main(int argc, char* argv[]) {
 			  << std::endl;
 
 	std::cout << "FFTW FFT-Pow2-convolution: " << std::flush;
-	auto result_fftw_pow2_conv = FftwConv( sig_data, ir_data);
+	auto result_fftw_pow2_conv = FftwConv(fft_sig, sig_vec, sig_re, sig_im, fft_ir, ir_vec, ir_re, ir_im, output);
 	AudioVec diff;
 	std::transform(
 			result_conv.second.begin()
